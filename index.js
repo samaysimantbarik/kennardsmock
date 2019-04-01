8eb931ff1f45788700f09c114e0ee897ecf35fa9
samaysimantbarik <44022711+samaysimantbarik@users.noreply.github.com>
2019-04-01 12:17:20 +1100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/customers",customers);
 app.use("/contracts",contracts);
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
  
 app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
