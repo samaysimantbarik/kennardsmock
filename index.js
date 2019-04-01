b79d86031334ba9778b3335b9774d5cba3cb0c6c
samaysimantbarik <44022711+samaysimantbarik@users.noreply.github.com>
2019-04-01 11:47:01 +1100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ app.use("/accounts",account);
 app.use("/customers",customers);
 app.use("/contracts",contracts);
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
 
 
-
-app.listen(3000, () => console.log("Listening"));
+//app.listen(3000, () => console.log("Listening"));
