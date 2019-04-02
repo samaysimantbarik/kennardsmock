da2f329ec2415556ec7cb5fee06374e500c6d3f4
samaysimant <samaysimant@gmail.com>
2019-04-02 12:36:47 +1100

    added all mocks

--- Services/search.js
+++ Services/search.js
@@ -128,3 +128,5 @@ router.get("/", (req,res) =>{
 
 
 });
+
+module.exports= router;
--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.use("/invoices",invoices);
 app.use("/payments",payments);
 app.use("/search",search);
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+// var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
  
 // app.listen(server_port, server_ip_address, function () {
 //   console.log( "Listening on " + server_ip_address + ", port " + server_port )
